98bbb92ca8748ce295c7a0c5d745b2fd25376b92
Akshay <Akshay Sharma>
2024-06-27 21:26:30 +0530

    fixed styling for table

--- src/routes/play.$roomId.lazy.tsx
+++ src/routes/play.$roomId.lazy.tsx
@@ -13,14 +13,14 @@ const GameComponent = () => {
       <div className="flex-1 flex item-center justify-center relative">
         <img className="h-[calc(100vh-5rem)]" src="/poker-table.png" />
 
-        <div className="clip-path-tl w-64 h-44 absolute bg-foreground top-[33%] -translate-y-[55%] left-[20%]"></div>
-        <div className="clip-path-horizontal w-64 h-44 absolute bg-foreground bottom-[40%] translate-y-[55%] rotate-180 left-[20%]"></div>
+        <div className="clip-path-tl xl:w-56 xl:h-40 w-56 h-40 absolute bg-foreground top-[33%] -translate-y-[55%] left-[20%]"></div>
+        <div className="clip-path-horizontal xl:w-56 xl:h-40 w-56 h-40 absolute bg-foreground bottom-[40%] translate-y-[55%] rotate-180 left-[20%]"></div>
 
-        <div className="clip-path-horizontal w-64 h-44 absolute bg-foreground top-[33%] -translate-y-[55%] right-[20%]"></div>
-        <div className="clip-path-tl w-64 h-44 absolute bg-foreground bottom-[40%] translate-y-[55%] rotate-180 right-[20%]"></div>
+        <div className="clip-path-horizontal xl:w-56 xl:h-40 w-56 h-40 absolute bg-foreground top-[33%] -translate-y-[55%] right-[20%]"></div>
+        <div className="clip-path-tl xl:w-56 xl:h-40 w-56 h-40 absolute bg-foreground bottom-[40%] translate-y-[55%] rotate-180 right-[20%]"></div>
 
-        <div className="w-44 h-44 bg-foreground absolute bottom-[40%] translate-y-[55%] left-[37%]"></div>
-        <div className="w-44 h-44 bg-foreground absolute bottom-[40%] translate-y-[55%] right-[37%]"></div>
+        <div className="xl:w-40 xl:h-40 w-40 h-40 bg-foreground absolute bottom-[40%] translate-y-[55%] left-[37%]"></div>
+        <div className="xl:w-40 xl:h-40 w-40 h-40 bg-foreground absolute bottom-[40%] translate-y-[55%] right-[37%]"></div>
 
         <div className="absolute bottom-2 w-[40rem] bg-foreground rounded shadow-sm h-20"></div>
       </div>
