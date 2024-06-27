6e9b38de387c29fcc598480981f23ff8eb9f7b71
Akshay <Akshay Sharma>
2024-06-27 16:36:37 +0530

    added apis for room and get room request

--- src/routes/play.$roomId.lazy.tsx
+++ src/routes/play.$roomId.lazy.tsx
@@ -1,14 +1,25 @@
 import Navbar from '@/components/navbar';
+import useProfile from '@/hooks/useProfile';
+import { GET_ROOMS_DETAILS } from '@/lib/constants';
+import { getRoomDetailService } from '@/services/room';
+import { useQuery } from '@tanstack/react-query';
 import { createLazyFileRoute, useParams } from '@tanstack/react-router';
 
 const GameComponent = () => {
   const { roomId } = useParams({ strict: false });
-
-  console.log(roomId);
-
+  const { roomOwner } = useProfile();
+  const { isLoading, data } = useQuery({
+    queryKey: [GET_ROOMS_DETAILS, { id: roomId ?? '' }],
+    queryFn: () => getRoomDetailService(roomId ?? ''),
+    enabled: !!roomId,
+    refetchInterval: 3000,
+    refetchIntervalInBackground: true,
+  });
+  console.log(data);
+  if (isLoading) return <div>Loading...</div>;
   return (
     <div className="flex flex-col h-screen bg-[url(/casino-bg.jpg)] bg-no-repeat bg-cover bg-center">
-      <Navbar />
+      <Navbar roomOwner={roomOwner} playersInRoom={data.players || []} playersRequested={data.playersRequested || []} />
 
       <div className="flex-1 flex item-center justify-center relative">
         <img className="h-[calc(100vh-5rem)]" src="/poker-table.png" />
--- src/services/room.ts
+++ src/services/room.ts
@@ -4,3 +4,44 @@ export const getGamebyOwnerService = async (id: string) => {
   const { data } = await http.get(`/gameroom/getbyowner/${id}`);
   return data;
 };
+
+
+export const getRoomDetailService = async (id: string) => {
+  const { data } = await http.get(`/gameroom/getdetails/${id}`);
+  return data;
+};
+
+export const getRooms = async (id: string) => {
+  const { data } = await http.get(`/gameroom/get?playerid=${id}`);
+  return data;
+};
+
+export const roomJoinRequest = async (roomId: string, userId: string) => {
+  const { data } = await http.post(`/gameroom/gamejoinrequest/${roomId}`, { user: userId });
+  return data;
+};
+
+export const roomJoinRequestAccept = async (roomId: string, userId: string) => {
+  const { data } = await http.post(`/gameroom/acceptrequest/${roomId}`, { user: userId });
+  return data;
+};
+
+export const updateRoom = async (id: string, game: unknown) => {
+  try {
+    const { data } = await http.patch(`/gameroom/update/${id}`, game);
+
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const rejectjoinrequestservice = async (id: string, game: unknown) => {
+  try {
+    const { data } = await http.patch(`/gameroom/rejectrequest/${id}`, game);
+
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
