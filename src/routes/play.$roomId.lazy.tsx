74cbade32aa0ceaf4b2f5a5097bb1e25e790fe29
itschirag-me <imchirag2000@gmail.com>
2024-06-27 21:15:42 +0530

    change position of blocks

--- src/routes/play.$roomId.lazy.tsx
+++ src/routes/play.$roomId.lazy.tsx
@@ -13,14 +13,14 @@ const GameComponent = () => {
       <div className="flex-1 flex item-center justify-center relative">
         <img className="h-[calc(100vh-5rem)]" src="/poker-table.png" />
 
-        <div className="clip-path-tl w-64 h-44 absolute bg-foreground top-[33%] -translate-y-1/2 left-[20%]"></div>
-        <div className="clip-path-horizontal w-64 h-44 absolute bg-foreground bottom-[40%] translate-y-1/2 rotate-180 left-[20%]"></div>
+        <div className="clip-path-tl w-64 h-44 absolute bg-foreground top-[33%] -translate-y-[55%] left-[16%]"></div>
+        <div className="clip-path-horizontal w-64 h-44 absolute bg-foreground bottom-[40%] translate-y-[55%] rotate-180 left-[16%]"></div>
 
-        <div className="clip-path-horizontal w-64 h-44 absolute bg-foreground top-[33%] -translate-y-1/2 right-[20%]"></div>
-        <div className="clip-path-tl w-64 h-44 absolute bg-foreground bottom-[40%] translate-y-1/2 rotate-180 right-[20%]"></div>
+        <div className="clip-path-horizontal w-64 h-44 absolute bg-foreground top-[33%] -translate-y-[55%] right-[16%]"></div>
+        <div className="clip-path-tl w-64 h-44 absolute bg-foreground bottom-[40%] translate-y-[55%] rotate-180 right-[16%]"></div>
 
-        <div className="w-44 h-44 bg-foreground absolute bottom-[40%] translate-y-1/2 left-[38%] drop-shadow-glow"></div>
-        <div className="w-44 h-44 bg-foreground absolute bottom-[40%] translate-y-1/2 right-[38%]"></div>
+        <div className="w-44 h-44 bg-foreground absolute bottom-[40%] translate-y-[55%] left-[36%]"></div>
+        <div className="w-44 h-44 bg-foreground absolute bottom-[40%] translate-y-[55%] right-[36%]"></div>
 
         <div className="absolute bottom-2 w-[40rem] bg-foreground rounded shadow-sm h-20"></div>
       </div>
