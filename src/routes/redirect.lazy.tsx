760f6360024206ea00bd0294a62cd1346ee23d1d
Akshay <Akshay Sharma>
2024-10-14 15:25:49 +0530

    change in redirect page

--- src/routes/redirect.lazy.tsx
+++ src/routes/redirect.lazy.tsx
@@ -1,5 +1,21 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
+import { useEffect, useState } from 'react';
 
+export default function PaymentRedirect() {
+  const [paymentStatus, setPaymentStatus] = useState('');
+  useEffect(() => {
+    function ExtractStatus() {
+      const url = window.location.href.split('?')[1];
+      if (url.split('=')[1] == '1') {
+        setPaymentStatus('1');
+      } else {
+        setPaymentStatus('0');
+      }
+    }
+    ExtractStatus();
+  });
+  return <h1>Redirect</h1>;
+}
 export const Route = createLazyFileRoute('/redirect')({
-  component: () => <div>Hello /redirect!</div>
-})
+  component: () => <PaymentRedirect />,
+});
