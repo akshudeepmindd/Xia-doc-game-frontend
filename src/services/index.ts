49007195c74668b68eeaf78254425a245c1898e1
akshudeepmindd <akshaymastrolinks@gmail.com>
2025-03-31 17:49:51 +0530

    Update index.ts

--- src/services/index.ts
+++ src/services/index.ts
@@ -5,7 +5,7 @@ import io from 'socket.io-client';
 export const socket = io('https://deepminddsvisualss.com/ws/');
 
 export const http = axios.create({
-  baseURL: "https://bizarre-sherline-devquatersinnovation-951c89ae.koyeb.app/api/",
+  baseURL: "https://xoc-dia-backend.onrender.com/api/",
 });
 
 /**
